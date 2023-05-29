a617aef6303d0928ae9819225096567ff113fe1d
eliasamaral <eliasseverianomaral@gmail.com>
2023-05-29 12:32:11 -0300

    adição de obras

--- src/db.js
+++ src/db.js
@@ -1,28 +1,485 @@
-export const obras =[
+export const obras = [
   {
-    projeto: 4001312054,
+    projeto: 4001439396,
+    diagrama: 400221179,
+    local: "Santa Luzia",
+    srv: [
+      {
+        codigo: 71010045,
+        descricao: "13.6 - ESTAI DE ÂNCORA",
+      },
+      {
+        codigo: 71010170,
+        descricao: "8.1 - I - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010195,
+        descricao: "8.6 - I - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71012164,
+        descricao: "I - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71016214,
+        descricao: "SERV I-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71010001,
+        descricao: "1.2 - PODA  ÁRVORE RURAL",
+      },
+      {
+        codigo: 71010075,
+        descricao: "2.1 - I - ATERRAMENTO DE CERCA",
+      },
+      {
+        codigo: 71011550,
+        descricao: "4.1.1 - I -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71011372,
+        descricao: "3.2 - I - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71010155,
+        descricao: "7.2 - I - ESTAÇÃO TRANSF MONO/ TRI",
+      },
+      {
+        codigo: 71010169,
+        descricao: "8.1 - D - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71012165,
+        descricao: "D - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71016210,
+        descricao: "SERV D-POSTE&lt;2500KG C/TRAN",
+      },
+    ],
+  },
+  {
+    projeto: 4001376530,
+    diagrama: 400216551,
     local: "Ibitirama",
     srv: [
       {
-        codigo: 71010010,
-        descricao: "POSTE",
+        codigo: 71010170,
+        descricao: "8.1 - I - ESTRUTURA PRIMÁRIA - MONO",
       },
       {
-        codigo: 31020120,
-        descricao: "Cabo",
+        codigo: 71011382,
+        descricao: "3.1 - I - CHAVE FACA",
       },
-      
+      {
+        codigo: 71011489,
+        descricao: "8.2.4-I-ESTR PRIMARIA-BIF/TRIF-TIPO4",
+      },
+      {
+        codigo: 71012164,
+        descricao: "I - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71011486,
+        descricao: "8.2.3-I-ESTR PRIMARIA-BIF/TRIF-TIPO3",
+      },
+      {
+        codigo: 71011550,
+        descricao: "4.1.1 - I -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71012314,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - I",
+      },
+      {
+        codigo: 71011372,
+        descricao: "3.2 - I - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71011488,
+        descricao: "8.2.2-I-ESTR PRIMARIA-BIF/TRIF-TIPO2",
+      },
+      {
+        codigo: 71010037,
+        descricao: "13.2 - CONCRETAR BASE",
+      },
+      {
+        codigo: 71010195,
+        descricao: "8.6 - I - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71016214,
+        descricao: "SERV I-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71016223,
+        descricao: "SERV I-POSTE&gt;=2500KG C/TRAN",
+      },
+      {
+        codigo: 71010040,
+        descricao: "13.5 - D - ESTAI",
+      },
+      {
+        codigo: 71010169,
+        descricao: "8.1 - D - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010194,
+        descricao: "8.6 - D - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71011371,
+        descricao: "3.2 - D - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71012165,
+        descricao: "D - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71016210,
+        descricao: "SERV D-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71011549,
+        descricao: "4.1.1 - D -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71012315,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - D",
+      },
     ],
   },
   {
-    projeto: 4001311234,
-    local: "Muniz",
+    projeto: 4001364035,
+    diagrama: 400215160,
+    local: "Ibitirama",
     srv: [
       {
-        codigo: 71010010,
-        descricao: "Concretar Base",
+        codigo: 71010155,
+        descricao: "7.2 - I - ESTAÇÃO TRANSF MONO/ TRI",
       },
-      
+      {
+        codigo: 71011488,
+        descricao: "8.2.2-I-ESTR PRIMARIA-BIF/TRIF-TIPO2",
+      },
+      {
+        codigo: 71012164,
+        descricao: "I - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71011486,
+        descricao: "8.2.3-I-ESTR PRIMARIA-BIF/TRIF-TIPO3",
+      },
+      {
+        codigo: 71011489,
+        descricao: "8.2.4-I-ESTR PRIMARIA-BIF/TRIF-TIPO4",
+      },
+      {
+        codigo: 71011550,
+        descricao: "4.1.1 - I -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71012314,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - I",
+      },
+      {
+        codigo: 71011372,
+        descricao: "3.2 - I - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71010001,
+        descricao: "1.2 - PODA  ÁRVORE RURAL",
+      },
+      {
+        codigo: 71010037,
+        descricao: "13.2 - CONCRETAR BASE",
+      },
+      {
+        codigo: 71010045,
+        descricao: "13.6 - ESTAI DE ÂNCORA",
+      },
+      {
+        codigo: 71010075,
+        descricao: "2.1 - I - ATERRAMENTO DE CERCA",
+      },
+      {
+        codigo: 71010170,
+        descricao: "8.1 - I - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010195,
+        descricao: "8.6 - I - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71011382,
+        descricao: "3.1 - I - CHAVE FACA",
+      },
+      {
+        codigo: 71012230,
+        descricao: "7.1 - R - TRANSFORMADOR - I",
+      },
+      {
+        codigo: 71016214,
+        descricao: "SERV I-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71010040,
+        descricao: "13.5 - D - ESTAI",
+      },
+      {
+        codigo: 71010154,
+        descricao: "7.2 - D - ESTAÇÃO TRANSF MONO / TRI",
+      },
+      {
+        codigo: 71010169,
+        descricao: "8.1 - D - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010194,
+        descricao: "8.6 - D - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71011371,
+        descricao: "3.2 - D - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71012165,
+        descricao: "D - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71012231,
+        descricao: "7.1 - R - TRANSFORMADOR - D",
+      },
+      {
+        codigo: 71016210,
+        descricao: "SERV D-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71011549,
+        descricao: "4.1.1 - D -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71012315,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - D",
+      },
     ],
   },
-]
+  {
+    projeto: 4001376556,
+    diagrama: 400216550,
+    local: "Ibitirama",
+    srv: [
+      {
+        codigo: 71011382,
+        descricao: "3.1 - I - CHAVE FACA",
+      },
+      {
+        codigo: 71011486,
+        descricao: "8.2.3-I-ESTR PRIMARIA-BIF/TRIF-TIPO3",
+      },
+      {
+        codigo: 71011489,
+        descricao: "8.2.4-I-ESTR PRIMARIA-BIF/TRIF-TIPO4",
+      },
+      {
+        codigo: 71012164,
+        descricao: "I - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71010101,
+        descricao: "4.15 - I - COND PRÉ-REUNIDA - SECUNDÁRIA",
+      },
+      {
+        codigo: 71011550,
+        descricao: "4.1.1 - I -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71010037,
+        descricao: "13.2 - CONCRETAR BASE",
+      },
+      {
+        codigo: 71010076,
+        descricao: "2.2 - I - ATERRAMENTO COM UMA HASTE",
+      },
+      {
+        codigo: 71010170,
+        descricao: "8.1 - I - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010195,
+        descricao: "8.6 - I - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71011372,
+        descricao: "3.2 - I - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71011488,
+        descricao: "8.2.2-I-ESTR PRIMARIA-BIF/TRIF-TIPO2",
+      },
+      {
+        codigo: 71012230,
+        descricao: "7.1 - R - TRANSFORMADOR - I",
+      },
+      {
+        codigo: 71016214,
+        descricao: "SERV I-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71010040,
+        descricao: "13.5 - D - ESTAI",
+      },
+      {
+        codigo: 71010169,
+        descricao: "8.1 - D - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010194,
+        descricao: "8.6 - D - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71011371,
+        descricao: "3.2 - D - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71016210,
+        descricao: "SERV D-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71012165,
+        descricao: "D - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71012231,
+        descricao: "7.1 - R - TRANSFORMADOR - D",
+      },
+      {
+        codigo: 71011549,
+        descricao: "4.1.1 - D -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71010045,
+        descricao: "13.6 - ESTAI DE ÂNCORA",
+      },
+      {
+        codigo: 71012315,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - D",
+      },
+    ],
+  },
+  {
+    projeto: 4001376475,
+    diagrama: 400216506,
+    local: "Ibitirama",
+    srv: [
+      {
+        codigo: 71010101,
+        descricao: "4.15 - I - COND PRÉ-REUNIDA - SECUNDÁRIA",
+      },
+      {
+        codigo: 71011488,
+        descricao: "8.2.2-I-ESTR PRIMARIA-BIF/TRIF-TIPO2",
+      },
+      {
+        codigo: 71011550,
+        descricao: "4.1.1 - I -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71012164,
+        descricao: "I - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71012314,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - I",
+      },
+      {
+        codigo: 71010170,
+        descricao: "8.1 - I - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71011372,
+        descricao: "3.2 - I - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71011486,
+        descricao: "8.2.3-I-ESTR PRIMARIA-BIF/TRIF-TIPO3",
+      },
+      {
+        codigo: 71011489,
+        descricao: "8.2.4-I-ESTR PRIMARIA-BIF/TRIF-TIPO4",
+      },
+      {
+        codigo: 71010037,
+        descricao: "13.2 - CONCRETAR BASE",
+      },
+      {
+        codigo: 71010123,
+        descricao: "4.9 - EMENDA",
+      },
+      {
+        codigo: 71010195,
+        descricao: "8.6 - I - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71012230,
+        descricao: "7.1 - R - TRANSFORMADOR - I",
+      },
+      {
+        codigo: 71016214,
+        descricao: "SERV I-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71010041,
+        descricao: "13.5 - I - ESTAI",
+      },
+      {
+        codigo: 71010040,
+        descricao: "13.5 - D - ESTAI",
+      },
+      {
+        codigo: 71010169,
+        descricao: "8.1 - D - ESTRUTURA PRIMÁRIA - MONO",
+      },
+      {
+        codigo: 71010194,
+        descricao: "8.6 - D - ESTRUTURA SECUNDÁRIA",
+      },
+      {
+        codigo: 71011371,
+        descricao: "3.2 - D - CHAVE FUSIVEL",
+      },
+      {
+        codigo: 71011484,
+        descricao: "8.2.4-D-ESTR PRIMARIA-BIF/TRIF-TIPO4",
+      },
+      {
+        codigo: 71016210,
+        descricao: "SERV D-POSTE&lt;2500KG C/TRAN",
+      },
+      {
+        codigo: 71012165,
+        descricao: "D - ATUALIZAÇÃO DA BASE CADASTRAL AS BUI",
+      },
+      {
+        codigo: 71012231,
+        descricao: "7.1 - R - TRANSFORMADOR - D",
+      },
+      {
+        codigo: 71011485,
+        descricao: "8.2.1-D-ESTR PRIMÁRIA - BIF/TRIF -TIPO 1",
+      },
+      {
+        codigo: 71011549,
+        descricao: "4.1.1 - D -COND. REDE CONV. ATÉ 1/0 AWG",
+      },
+      {
+        codigo: 71012315,
+        descricao: "4.1 - R - CONDUTOR CONV. ATÉ 1/0 AWG - D",
+      },
+      {
+        codigo: 71010045,
+        descricao: "13.6 - ESTAI DE ÂNCORA",
+      },
+    ],
+  },
+];
