b268c93183430eaaf80bbedca3653364eecec388
Elias Amaral <eliasseverianoamaral@gmail.com>
2024-03-02 12:04:12 -0300

    configuração de cach e devTools do apollo

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,11 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const client = new ApolloClient({
   uri: API_URL,
-  cache: new InMemoryCache({addTypename: false}),
-  connectToDevTools: import.meta.env.MODE === "develop"
-
-  
+  cache: new InMemoryCache(),
+  connectToDevTools: process.env.NODE_ENV === "development",
 });
