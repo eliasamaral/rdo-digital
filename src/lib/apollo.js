dfb7c32785b8d81f6c5bf21c60fcc445069accfb
Elias Severiano Amaral <eliasseverianoamaral@gmail.com>
2024-09-06 16:33:56 -0300

    add .env

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,9 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const client = new ApolloClient({
-  uri: API_URL,
+  uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache({addTypename: false}),
   connectToDevTools: process.env.NODE_ENV === "development",
 });
