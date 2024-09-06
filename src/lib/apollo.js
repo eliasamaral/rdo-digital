f91c60d0d7493ab933db81fabaa4155043616fbf
Elias Severiano Amaral <eliasseverianoamaral@gmail.com>
2024-09-06 15:39:11 -0300

    adiciona variavel ambiente

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const client = new ApolloClient({
-  uri: "https://manager-api-production.up.railway.app",
+  uri: API_URL,
   cache: new InMemoryCache({addTypename: false}),
   connectToDevTools: process.env.NODE_ENV === "development",
 });
