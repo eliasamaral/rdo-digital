272187526b34187d87e83bbab3eb4f7aa5d52fbf
Elias Severiano Amaral <eliasseverianoamaral@gmail.com>
2024-09-06 21:33:44 -0300

    biomejs config

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,10 +1,9 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 export const client = new ApolloClient({
-  uri: API_URL,
-  cache: new InMemoryCache({addTypename: false}),
-  connectToDevTools: process.env.NODE_ENV === "development",
-});
-
+	uri: API_URL,
+	cache: new InMemoryCache({ addTypename: false }),
+	connectToDevTools: process.env.NODE_ENV === 'development',
+})
